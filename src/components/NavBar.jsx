6a4d2886235286247c77ae7164c852b6aea53b28
Mariana Camacho <marianacamacho700@gmail.com>
2022-06-13 10:19:07 -0500

    responsive menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,23 +10,44 @@ const NavBar = () => {
 
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" expand="lg" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
-            <img className="bi me-2" src="https://res.cloudinary.com/dxvzsg7fa/image/upload/v1654989867/Rick-and-Morty_kt4kt8.png" width={150} alt="logo" />
+            <img
+              className="bi me-2"
+              src="https://res.cloudinary.com/dxvzsg7fa/image/upload/v1654989867/Rick-and-Morty_kt4kt8.png"
+              width={150}
+              alt="logo"
+            />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Link className="nav-link text-uppercase fw-bold" to="/">Home</Link>
-            {user.isAuthenticated ?<></>:<Link className="nav-link text-uppercase fw-bold" to="/login">Login</Link>}
-            <Link className="nav-link text-uppercase fw-bold" to="/registro">Registro</Link>
-            <button
-              className="nav-link text-uppercase fw-bold text-decoration-none bg-transparent"
-              style={{ display: user?.isAuthenticated ? "block" : "none", border: "none"}}
-              onClick={() => dispatch(logout())}
-            >
-              Logout
-            </button>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link className="nav-link text-uppercase fw-bold" to="/">
+                Home
+              </Link>
+              {user.isAuthenticated ? (
+                <></>
+              ) : (
+                <Link className="nav-link text-uppercase fw-bold" to="/login">
+                  Login
+                </Link>
+              )}
+              <Link className="nav-link text-uppercase fw-bold" to="/registro">
+                Registro
+              </Link>
+              <button
+                className="nav-link text-uppercase fw-bold text-decoration-none bg-transparent"
+                style={{
+                  display: user?.isAuthenticated ? "block" : "none",
+                  border: "none",
+                }}
+                onClick={() => dispatch(logout())}
+              >
+                Logout
+              </button>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
